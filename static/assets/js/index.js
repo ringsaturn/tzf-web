ef471167daed4a963a4dcc35844f727b761e017e
Han Xiao <ringsaturn.me@gmail.com>
2026-04-26 01:08:14 +0900

    bump

--- static/assets/js/index.js
+++ static/assets/js/index.js
@@ -1,4 +1,4 @@
-import init, { WasmFinder } from "https://unpkg.com/tzf-wasm@1.1.4/tzf_wasm.js";
+import init, { WasmFinder } from "https://unpkg.com/tzf-wasm@1.2.0/tzf_wasm.js";
 import {
   formatTime,
   getSolarTime,
